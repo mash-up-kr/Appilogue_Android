58121f9715495d4cd7681aaed06177f997e7a3d5
junbi-kim <junbi.kim@navercorp.com>
2021-10-02 18:30:28 +0900

    [#15] 베이스 컬러, 테마 정리

--- app/src/main/java/com/anonymous/appilogue/AppilogueApplication.kt
+++ app/src/main/java/com/anonymous/appilogue/AppilogueApplication.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class AnonymousApplication : Application() {
+class AppilogueApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
