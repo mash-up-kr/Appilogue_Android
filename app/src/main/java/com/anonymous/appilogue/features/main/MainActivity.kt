f24c6573df9340e6e2e42bd18654c6a9ac076684
KimReady <kjb4380@gmail.com>
2021-10-23 20:22:24 +0900

    머지 충돌 해결

--- app/src/main/java/com/anonymous/appilogue/features/main/MainActivity.kt
+++ app/src/main/java/com/anonymous/appilogue/features/main/MainActivity.kt
@@ -1,25 +1,14 @@
 package com.anonymous.appilogue.features.main
 
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
 import androidx.activity.viewModels
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.navigation.NavHost
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.lifecycle.lifecycleScope
 import com.anonymous.appilogue.R
 import com.anonymous.appilogue.databinding.ActivityMainBinding
 import com.anonymous.appilogue.features.base.BaseActivity
-import com.anonymous.appilogue.features.home.HomeFragment
-import com.anonymous.appilogue.features.home.HomeViewModel
-import com.anonymous.appilogue.features.profile.ProfileFragment
 import com.anonymous.appilogue.features.search.AppSearchManager
-import com.anonymous.appilogue.features.search.SearchAppFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -29,6 +18,9 @@ import javax.inject.Inject
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     val viewModel: MainViewModel by viewModels()
+
+    @Inject
+    lateinit var appSearchManager: AppSearchManager
 
     private val navController by lazy {
         val navHostFragment =
