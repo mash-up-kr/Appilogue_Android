d61bee2c5bb807f9eddaf25ed4cb7f126e51e78a
KangDahyun <dahyun1226@naver.com>
2021-10-29 06:05:21 +0900

    Move ellipsizeText & hasEllipsized to TextUtil

--- app/src/main/java/com/anonymous/appilogue/features/notification/NotificationBinding.kt
+++ app/src/main/java/com/anonymous/appilogue/features/notification/NotificationBinding.kt
@@ -1,6 +1,5 @@
 package com.anonymous.appilogue.features.notification
 
-import android.graphics.Paint
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
@@ -10,6 +9,7 @@ import androidx.databinding.BindingAdapter
 import com.anonymous.appilogue.R
 import com.anonymous.appilogue.model.Notification
 import com.anonymous.appilogue.utils.dateToPassedTime
+import com.anonymous.appilogue.utils.ellipsizeText
 
 
 @BindingAdapter("notificationText")
@@ -28,7 +28,8 @@ fun TextView.setNotificationText(notification: Notification) {
                 width,
                 textSize,
                 maxLines,
-                notification.date.dateToPassedTime(context)
+                notification.date.dateToPassedTime(context),
+                ELLIPSIZE_NUM
             )
             text.toString().replace(" ", "\u00A0")
             text = SpannableString(ellipsizedText)
@@ -50,39 +51,4 @@ fun TextView.setNotificationText(notification: Notification) {
     })
 }
 
-private fun hasEllipsized(
-    text: String,
-    tvWidth: Int,
-    textSize: Float,
-    maxLines: Int
-): Boolean {
-    val paint = Paint()
-    paint.textSize = textSize
-    val size = paint.measureText(text).toInt()
-    return tvWidth * maxLines < size
-}
-
-private fun ellipsizeText(
-    rawText: String,
-    width: Int,
-    textSize: Float,
-    maxLines: Int,
-    lastText: String
-): String {
-    val isEllipsis = hasEllipsized(rawText, width, textSize, maxLines)
-    if (isEllipsis) {
-        var ellipsisText = rawText.replace(" ", "\u00A0")
-        while (hasEllipsized(ellipsisText, width, textSize, maxLines)) {
-            ellipsisText = ellipsisText.substring(0, ellipsisText.length - 1).trim()
-        }
-        return "${
-            ellipsisText.substring(
-                0,
-                ellipsisText.length - ELLIPSIZE_NUM - lastText.length - ADDITIONAL_TEXT_SPACE
-            )
-        }${".".repeat(ELLIPSIZE_NUM)} $lastText"
-    } else return rawText.replace(" ", "\u00A0")
-}
-
 const val ELLIPSIZE_NUM = 3
-const val ADDITIONAL_TEXT_SPACE = 5
