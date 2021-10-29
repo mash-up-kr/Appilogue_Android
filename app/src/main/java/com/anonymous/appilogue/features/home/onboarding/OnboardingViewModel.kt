3ece64ebe630a5a6990d16d9df54ae84cab39ebd
KangDahyun <dahyun1226@naver.com>
2021-10-30 01:57:24 +0900

    Set Onboarding Dialog UI

--- app/src/main/java/com/anonymous/appilogue/features/home/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/anonymous/appilogue/features/home/onboarding/OnboardingViewModel.kt
@@ -1,6 +1,14 @@
 package com.anonymous.appilogue.features.home.onboarding
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OnboardingViewModel : ViewModel() {
+    private val _description = MutableLiveData(EMPTY_STRING)
+    val description: LiveData<String> = _description
+
+    companion object {
+        const val EMPTY_STRING = ""
+    }
 }
