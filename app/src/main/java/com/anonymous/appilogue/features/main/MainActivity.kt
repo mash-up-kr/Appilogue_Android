357e410619bf1b5395778f15acee75a7bab82dfb
seokjoo <tjrwn0716@naver.com>
2021-12-10 15:30:12 +0900

    뒤로가기 버튼 2번 클릭시 종료

--- app/src/main/java/com/anonymous/appilogue/features/main/MainActivity.kt
+++ app/src/main/java/com/anonymous/appilogue/features/main/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.anonymous.appilogue.features.main
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.view.forEach
-import androidx.navigation.fragment.NavHostFragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDirections
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.anonymous.appilogue.R
 import com.anonymous.appilogue.databinding.ActivityMainBinding
 import com.anonymous.appilogue.features.base.BaseActivity
 import com.anonymous.appilogue.features.home.Focus
-import com.anonymous.appilogue.features.home.HomeFragment
 import com.anonymous.appilogue.features.home.HomeViewModel
 import com.anonymous.appilogue.features.home.bottomsheet.space_dust.MySpaceDustViewModel
 import com.anonymous.appilogue.features.search.AppSearchManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
+import kotlin.system.exitProcess
 
 
 @AndroidEntryPoint
@@ -30,6 +31,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     val viewModel: MainViewModel by viewModels()
     val mySpaceDustViewModel: MySpaceDustViewModel by viewModels()
     val homeViewModel: HomeViewModel by viewModels()
+
+    private var disposable: Disposable? = null
+    private val behaviorSubject = BehaviorSubject.createDefault(0L)
 
     @Inject
     lateinit var appSearchManager: AppSearchManager
@@ -47,6 +51,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         }
         initNavigation()
         viewModel.fetchMyInformation()
+        doubleClickBackPressed()
+    }
+
+    private fun doubleClickBackPressed() {
+        disposable = behaviorSubject.buffer(2, 1)
+            .map { it[0] to it[1] }
+            .subscribe {
+                if (it.second - it.first < 2000L) {
+                    super.onBackPressed()
+                    moveTaskToBack(true)
+                    this.finishAndRemoveTask()
+                    exitProcess(0)
+                } else {
+                    Toast.makeText(this, "앱을 종료하려면 한번 더 눌러주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     private fun initNavigation() {
@@ -108,9 +128,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 }
             }
         }
-        super.onBackPressed()
+        behaviorSubject.onNext(System.currentTimeMillis())
+
         navController.currentDestination?.let {
             changeBottomNavigationStateByFragmentId(it.id)
         }
+    }
+
+    override fun onDestroy() {
+        disposable?.dispose()
+        super.onDestroy()
     }
 }
